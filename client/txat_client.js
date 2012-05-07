390f8e1a3bbeb760d131dc2330ab124fafa63132
Jimakker <tiburtzio@gmail.com>
2012-05-07 14:59:37 +0200

    jquery namespacea zuzendua

--- client/txat_client.js
+++ client/txat_client.js
@@ -1,6 +1,5 @@
 var socketT = io.connect('http://[zure-helbidea]:[portua]');
 moment.lang('eu');
-var $j = jQuery.noConflict();
 var barruan = false;
 
 var nickname="";
@@ -13,16 +12,16 @@ var initialPageTitle = document.title;
 var isWindowActive = false;
 
 
-$j(window).focus(function(){
+$(window).focus(function(){
 	isWindowActive = true;
-	if(barruan&&$j('#txat-wrapper').css('display')!='none'){
+	if(barruan&&$('#txat-wrapper').css('display')!='none'){
 		document.title=initialPageTitle;
-		$j('.txat-button-count').html("");
+		$('.txat-button-count').html("");
 	}
 });
 
 
-$j(window).blur(function(){
+$(window).blur(function(){
 	isWindowActive = false;
 });
 
@@ -38,15 +37,15 @@ socketT.on('chatNew', function(data){
 		document.title= "("+noReadMsg+") "+initialPageTitle;
 	}
 
-        if(barruan&&$j('#txat-wrapper').css('display')=='none'){
+        if(barruan&&$('#txat-wrapper').css('display')=='none'){
 	if(!isAlreadyDone){
             noReadMsg++;
 	}
-            $j('#txat-button-visual').stop().animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 );
-            $j('.txat-button-count').html("("+noReadMsg+")");
+            $('#txat-button-visual').stop().animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 );
+            $('.txat-button-count').html("("+noReadMsg+")");
 		
         }else{
-          $j('#txat-button-visual').stop().animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 );
+          $('#txat-button-visual').stop().animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 ).animate({marginRight: "10px"}, 50 ).animate({marginRight: "0px"}, 50 );
 
         }
 
@@ -54,13 +53,13 @@ socketT.on('chatNew', function(data){
         
         if(data[2]==lastChatLineUser){
           
-          $j('#chat-ul li:first-child .date').attr('title',date).text('oraintxe');
-          $j('#chat-ul li:first-child').append('<span class="txat-msg msg'+count+'">'+urlify(data[3])+'</span>');
-          $j('.msg'+count).emoticonize();
+          $('#chat-ul li:first-child .date').attr('title',date).text('oraintxe');
+          $('#chat-ul li:first-child').append('<span class="txat-msg msg'+count+'">'+urlify(data[3])+'</span>');
+          $('.msg'+count).emoticonize();
         } else{
           lastChatLineUser=data[2];
-          $j('#chat-ul').prepend('<li style="padding:5px;border:1px solid #ddd;margin-bottom:3px;" class="txat-linea txat'+count+'"><span class="date"  title="'+date+'">oraintxe</span><span class="txat-user" title="erantzun erabiltzailea honi" onclick="erantzunHoni(this);">'+ data[2] +'</span><br><span class="txat-msg msg'+count+'">'+urlify(data[3])+'</span></li>');
-          $j('.msg'+count).emoticonize();
+          $('#chat-ul').prepend('<li style="padding:5px;border:1px solid #ddd;margin-bottom:3px;" class="txat-linea txat'+count+'"><span class="date"  title="'+date+'">oraintxe</span><span class="txat-user" title="erantzun erabiltzailea honi" onclick="erantzunHoni(this);">'+ data[2] +'</span><br><span class="txat-msg msg'+count+'">'+urlify(data[3])+'</span></li>');
+          $('.msg'+count).emoticonize();
         }
 
         
@@ -72,15 +71,15 @@ socketT.on('chatLog', function(data){
   for(var i=0;i<data.length;i++){
         var date = new Date(data[i]['date']);
     if(data[i]['user']==lastChatLineUser){
-        $j('#chat-ul li:first-child .date').attr('title',date).text('oraintxe');
-          $j('#chat-ul li:first-child').append('<span class="txat-msg msg'+count+'">'+urlify(data[i]['msg'])+'</span>');
+        $('#chat-ul li:first-child .date').attr('title',date).text('oraintxe');
+          $('#chat-ul li:first-child').append('<span class="txat-msg msg'+count+'">'+urlify(data[i]['msg'])+'</span>');
 
     }else{
-      $j('#chat-ul').prepend('<li style="opacity:.7;padding:5px;border:1px solid #ddd;margin-bottom:3px;" class="txat-linea txat'+count+'"><span class="date" title="'+date+'">'+date.toGMTString()+'</span><span title="erantzun erabiltzailea honi" class="txat-user" onclick="erantzunHoni(this);">'+data[i]['user']+'</span><br><span class="txat-msg msg'+count+'">'+urlify(data[i]['msg'])+'</span></li>');
+      $('#chat-ul').prepend('<li style="opacity:.7;padding:5px;border:1px solid #ddd;margin-bottom:3px;" class="txat-linea txat'+count+'"><span class="date" title="'+date+'">'+date.toGMTString()+'</span><span title="erantzun erabiltzailea honi" class="txat-user" onclick="erantzunHoni(this);">'+data[i]['user']+'</span><br><span class="txat-msg msg'+count+'">'+urlify(data[i]['msg'])+'</span></li>');
       lastChatLineUser=data[i]['user'];
     }
-  $j('.msg'+count).emoticonize();
-	if(data[i][2]==nickname){$j('.txat'+count).addClass('mine');};
+  $('.msg'+count).emoticonize();
+	if(data[i][2]==nickname){$('.txat'+count).addClass('mine');};
 	
 	count++;
 	        
@@ -91,23 +90,23 @@ socketT.on('chatLog', function(data){
 
 socketT.on('userCount',function(data){
 //  console.log("erailtzaileak:" +data);
-	$j('#txat-stats').html('<ul style="margin-top:5px;"><li>Txat erabiltzaileak orain: '+data+'</li></ul>');
+	$('#txat-stats').html('<ul style="margin-top:5px;"><li>Txat erabiltzaileak orain: '+data+'</li></ul>');
 
 });
 
 socketT.on('barrura_sartu',function(){
-  $j('#txat-sarrera-testua').hide();
-  $j('#chatPwd').remove();
+  $('#txat-sarrera-testua').hide();
+  $('#chatPwd').remove();
 
-  $j('#txat-wrapper').prepend("<span style='padding:5px;border-top-right-radius:5px;border-top-left-radius:5px;background: #333;color:#fff;height: 20px;display: block;'><img align='center' src='http://ttanttakun.comjs/txat/user.png'/>"+nickname+"<span style='cursor:pointer;float:right;' onclick='$j(\"#txat-wrapper\").slideToggle()'>X</span></span>");
+  $('#txat-wrapper').prepend("<span style='padding:5px;border-top-right-radius:5px;border-top-left-radius:5px;background: #333;color:#fff;height: 20px;display: block;'><img align='center' src='http://ttanttakun.comjs/txat/user.png'/>"+nickname+"<span style='cursor:pointer;float:right;' onclick='$(\"#txat-wrapper\").slideToggle()'>X</span></span>");
  // console.log("received OK from server");
 	localStorage.txat=nickname;
   
         barruan=true;
-        $j('#chatMsg').val("");
-        $j('#chatMsg').prop('placeholder','Mezua idatzi');
-        $j('#chatBidaliBotoia').prop('value','Bidali');
-  $j("#chat-ul").css('height',430).prepend('<li style="padding:5px;background:#ddd;margin-bottom:3px;border:1px solid #333;" class="txat-linea txat'+count+'"><b>Kaixo!</b> Hau TtanttaTxata da! Frogatan ari gera oraindikan, beraz, arazoak sor ditzake eta barkaiguzue horregatik. Guztiz PUBLIKOA da, ez jarri datu pribaturik ಠ_ಠ</li>');
+        $('#chatMsg').val("");
+        $('#chatMsg').prop('placeholder','Mezua idatzi');
+        $('#chatBidaliBotoia').prop('value','Bidali');
+  $("#chat-ul").css('height',430).prepend('<li style="padding:5px;background:#ddd;margin-bottom:3px;border:1px solid #333;" class="txat-linea txat'+count+'"><b>Kaixo!</b> Hau TtanttaTxata da! Frogatan ari gera oraindikan, beraz, arazoak sor ditzake eta barkaiguzue horregatik. Guztiz PUBLIKOA da, ez jarri datu pribaturik ಠ_ಠ</li>');
 count++;
 lastChatLineUser="";
 });
@@ -125,7 +124,7 @@ socketT.on('sartzerakoan_errorea',function(){
 socketT.on('erabiltzaile_berria',function(data,zenbat){
 //console.log(data);
 //console.log(zenbat);
-	$j("#chat-ul").prepend('<li style="padding:5px;background:LightSteelBlue;margin-bottom:3px;" class="txat-linea txat'+count+'">Erabiltzaile konektatua: <b>'+ data +'</b></li>');
+	$("#chat-ul").prepend('<li style="padding:5px;background:LightSteelBlue;margin-bottom:3px;" class="txat-linea txat'+count+'">Erabiltzaile konektatua: <b>'+ data +'</b></li>');
 count++;
 lastChatLineUser="";
 });
@@ -134,23 +133,23 @@ lastChatLineUser="";
 socketT.on('erabiltzaile_deskonektatua',function(data,zenbat){
  // console.log(zenbat);
 lastChatLineUser="";
-  $j("#chat-ul").prepend('<li class="chat-line info-linea" style="padding:5px;background:Salmon;margin-bottom:3px;" class="txat-linea txat'+count+'">Erabiltzaile  deskonektatua: <b>'+ data +'</b></li>');
+  $("#chat-ul").prepend('<li class="chat-line info-linea" style="padding:5px;background:Salmon;margin-bottom:3px;" class="txat-linea txat'+count+'">Erabiltzaile  deskonektatua: <b>'+ data +'</b></li>');
 count++;
 
 });
 
 function sendChat(){
         var cDate = new Date();
-        var msg = $j('#chatMsg').val();
+        var msg = $('#chatMsg').val();
         if(nickname==lastChatLineUser){
-          $j('#chat-ul li:first-child .date').attr('title',cDate).text('oraintxe');
-          $j('#chat-ul li:first-child').append('<span class="txat-msg msg'+count+'">'+urlify(msg)+'</span>');
+          $('#chat-ul li:first-child .date').attr('title',cDate).text('oraintxe');
+          $('#chat-ul li:first-child').append('<span class="txat-msg msg'+count+'">'+urlify(msg)+'</span>');
         }else{
           lastChatLineUser=nickname;
-          $j("#chat-ul").prepend('<li style="padding:5px;background:#ccffce;margin-bottom:3px;" class="txat-linea txat'+count+'"><span class="date" title="'+cDate+'" >oraintxe</span><span class="txat-user"  onclick="erantzunHoni(this);">'+nickname+'</span><br><span class="txat-msg msg'+count+'">'+urlify(msg)+'</span></li>');
+          $("#chat-ul").prepend('<li style="padding:5px;background:#ccffce;margin-bottom:3px;" class="txat-linea txat'+count+'"><span class="date" title="'+cDate+'" >oraintxe</span><span class="txat-user"  onclick="erantzunHoni(this);">'+nickname+'</span><br><span class="txat-msg msg'+count+'">'+urlify(msg)+'</span></li>');
         }
-       $j('#chatMsg').val('');
-	     $j('.msg'+count).emoticonize({animate:true});
+       $('#chatMsg').val('');
+	     $('.msg'+count).emoticonize({animate:true});
 	     count++;
        socketT.emit('chatMsg',cDate,encodeHTML(msg));
 }
@@ -169,15 +168,15 @@ function urlify(text) {
 
 
 
-setTimeout(function() {$j('#txat-wrapper .date').each(function(index){
+setTimeout(function() {$('#txat-wrapper .date').each(function(index){
   
-  $j(this).text(moment(new Date($j(this).attr('title'))).fromNow());
+  $(this).text(moment(new Date($(this).attr('title'))).fromNow());
 
 })}, 2000);
 
-setInterval(function() {$j('#txat-wrapper .date').each(function(index){
+setInterval(function() {$('#txat-wrapper .date').each(function(index){
   
-  $j(this).text(moment(new Date($j(this).attr('title'))).fromNow());
+  $(this).text(moment(new Date($(this).attr('title'))).fromNow());
 
 })}, 30000);
 
@@ -199,9 +198,9 @@ function initTxat(){
 function sendKaixoMessage(){
   var cDate = new Date();
   
-  nickname= encodeHTML($j('#chatMsg').val());
+  nickname= encodeHTML($('#chatMsg').val());
 
-  socketT.emit("kaixo",nickname,$j('#chatPwd').val(),true);
+  socketT.emit("kaixo",nickname,$('#chatPwd').val(),true);
   
   
 }
@@ -209,13 +208,13 @@ function sendKaixoMessage(){
 function sendForm(){
   
     if(barruan){
-      if($j('#chatMsg').val()){
+      if($('#chatMsg').val()){
         sendChat();
       }
     } else {
       
-      if($j('#chatMsg').val()){
-        nickname= $j('#chatMsg').val();
+      if($('#chatMsg').val()){
+        nickname= $('#chatMsg').val();
         
         sendKaixoMessage();
                
@@ -224,7 +223,7 @@ function sendForm(){
 }
 
 function erantzunHoni(userNameSpan){
-  $j('#chatMsg').val("@"+$j(userNameSpan).text()+" ");
+  $('#chatMsg').val("@"+$(userNameSpan).text()+" ");
 
 }
 
